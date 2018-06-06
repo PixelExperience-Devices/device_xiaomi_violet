e7bb83c32fbc58eab1dc9e982d9eb959fcbfcf8d
vasishath <vasishath@gmail.com>
2018-06-06 08:57:37 -0400

    sm6150-common: Detect maximum brightness dynamically
     * Instead of hardcoding the value.
    
    Change-Id: I9385be40a8598e1142bb3844446b6bbeb5aab008

--- light/Light.cpp
+++ light/Light.cpp
@@ -27,11 +27,9 @@
 
 #define BREATH          "breath"
 #define BRIGHTNESS      "brightness"
+#define MAX_BRIGHTNESS  "max_brightness"
 #define DELAY_OFF       "delay_off"
 #define DELAY_ON        "delay_on"
-
-#define MAX_LED_BRIGHTNESS    255
-#define MAX_LCD_BRIGHTNESS    4095
 
 namespace {
 /*
@@ -50,6 +48,25 @@ static void set(std::string path, std::string value) {
 
 static void set(std::string path, int value) {
     set(path, std::to_string(value));
+}
+
+static int get(std::string path) {
+    std::ifstream file(path);
+    int value;
+
+    if (!file.is_open()) {
+    ALOGW("failed to read from %s", path.c_str());
+    return 0;
+    }
+
+    file >> value;
+    return value;
+}
+
+static int getMaxBrightness(std::string path) {
+    int value = get(path);
+    ALOGW("Got max brightness %d", value);
+    return value;
 }
 
 static uint32_t getBrightness(const LightState& state) {
@@ -84,12 +101,12 @@ static inline uint32_t getScaledBrightness(const LightState& state, uint32_t max
 }
 
 static void handleBacklight(const LightState& state) {
-    uint32_t brightness = getScaledBrightness(state, MAX_LCD_BRIGHTNESS);
+    uint32_t brightness = getScaledBrightness(state, getMaxBrightness(LCD_LED MAX_BRIGHTNESS));
     set(LCD_LED BRIGHTNESS, brightness);
 }
 
 static void handleNotification(const LightState& state) {
-    uint32_t whiteBrightness = getScaledBrightness(state, MAX_LED_BRIGHTNESS);
+    uint32_t whiteBrightness = getScaledBrightness(state, getMaxBrightness(WHITE_LED MAX_BRIGHTNESS));
 
     /* Disable breathing */
     set(WHITE_LED BREATH, 0);
