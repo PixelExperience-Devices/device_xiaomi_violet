04a3ecf260eaa3a8c6820d83d944170c22f4cde0
Ramkumar Radhakrishnan <ramkumar@codeaurora.org>
2018-07-26 14:20:42 -0700

    sm6150-common: init: early_boot: Add permissions to wled/lcd_bias secure_mode node
    Add read/write permissions to system graphics group for wled/lcd_bias
    secure_mode node to notify secure display start/end to PMIC driver.
    
    Change-Id: I5f35080940b400de66d84617e08d590dd4ab7783
    CRs-Fixed: 2289552

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -421,6 +421,10 @@ else
     set_perms /sys/devices/virtual/hdcp/msm_hdcp/min_level_change system.graphics 0660
 fi
 
+# allow system_graphics group to access pmic secure_mode node
+set_perms /sys/class/lcd_bias/secure_mode system.graphics 0660
+set_perms /sys/class/leds/wled/secure_mode system.graphics 0660
+
 boot_reason=`cat /proc/sys/kernel/boot_reason`
 reboot_reason=`getprop ro.boot.alarmboot`
 if [ "$boot_reason" = "3" ] || [ "$reboot_reason" = "true" ]; then
