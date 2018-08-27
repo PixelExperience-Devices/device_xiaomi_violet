77e550da62777abca2ff27a4a9a1e799d052679f
Sandeep Singh <sandsing@codeaurora.org>
2018-08-27 11:31:18 +0530

    sm6150-common: wifi: Add target specific wifi configuration files
    This commit imports initial talos configuration files.
    
    Change-Id: I5aa01eb778bf24cf50421c093c9ebca4dd541ae0
    Signed-off-by: Sandeep Singh <sandsing@codeaurora.org>

--- sm6150.mk
+++ sm6150.mk
@@ -380,6 +380,14 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.wifi.hostapd@1.0.vendor \
     vendor.qti.hardware.wifi.supplicant@2.0.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/hostapd.accept:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.accept \
+    $(LOCAL_PATH)/wifi/hostapd.conf:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd_default.conf \
+    $(LOCAL_PATH)/wifi/hostapd.deny:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.deny \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
 # WiFi Display
 PRODUCT_BOOT_JARS += \
     WfdCommon
