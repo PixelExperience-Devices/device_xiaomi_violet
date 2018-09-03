862ed58dc17144ae8a3b8cec7e22c9537b5b7423
Anjaneya Prasad Musunuri <aprasad@codeaurora.org>
2018-09-03 13:09:41 +0530

    sm6150-common: display: Move node permissions from init to rc file. Permissions for Nodes declared in init process will be set during boot time, whereas nodes declared in uevent will get during creation of nodes. This will save boot up time.
    CRs-Fixed: 2457603
    Change-Id: Ie9b423d7f95845930f9a84c20cd52e04051628ce

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -1,6 +1,6 @@
 #! /vendor/bin/sh
 
-# Copyright (c) 2012-2013,2016,2018 The Linux Foundation. All rights reserved.
+# Copyright (c) 2012-2013,2016,2018,2019 The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
