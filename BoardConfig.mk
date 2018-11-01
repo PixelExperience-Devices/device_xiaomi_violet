f2ed24861176adce8684093a2975c554509db089
argraur <argraur@gmail.com>
2018-11-01 18:10:05 +0000

    violet: arch: cortex-a75 is armv8-2a and armv8-a
    v8.2-A improvements
    Version 8.2-A includes enhanced memory model,
    half-precision floating point data processing
    and introduces both RAS (reliability availability serviceability) support
    and statistical profiling extension (SPE).

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,7 +10,7 @@ DEVICE_PATH := device/xiaomi/violet
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := kryo300
