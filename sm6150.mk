17b54841b56b38e2be091fd43aa18217657669bd
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    sm6150-common: Enforce RRO for all resource overlays
    This configuration will likely go in mainline_system configuration.
    
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- sm6150.mk
+++ sm6150.mk
@@ -20,8 +20,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Properties
 -include $(LOCAL_PATH)/common-props.mk
