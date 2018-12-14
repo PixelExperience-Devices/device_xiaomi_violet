f37dbb2199686ee72555f929fccfac90d277b7f4
YH_Lin <yhli@google.com>
2018-12-14 13:54:23 +0800

    sm6150-common: remove mounting persist partition via fstab.persist
    Change to mount persist partition via kernel device tree in boot up
    progress.
    
    Bug: 119959876
    Test: build pass, boot to home
    Change-Id: Ic0e56280538b4130d1e51eeef461508e3f6021b5
    Signed-off-by: YH_Lin <yhli@google.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -69,14 +69,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.persist
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.persist
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.davinci.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
--- sm6150.mk
+++ sm6150.mk
@@ -355,7 +355,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     ueventd.qcom.rc \
-    fstab.persist \
     fstab.qcom
 
 # RCS
