db69c5b5095e5b8bb06d5d04dc6360923e0a6145
Sundong Ahn <sundongahn@google.com>
2018-12-17 16:40:39 +0900

    sm6150-common: Add system properties for surfaceflinger
    The configstore will be not used in surfaceflinger. So surfaceflinger
    will use the system properties instead of configstore.
    For this, the system properties are added.
    
    Bug: 112386364
    Test: m -j && boot test
    Change-Id: Ie3d2adccca06702217984cf2c9db986d852b7014

--- common-props.mk
+++ common-props.mk
@@ -123,6 +123,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.egl=adreno \
     ro.opengles.version=196610
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+
 # IO CGroup
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.iocgrp.config=1
