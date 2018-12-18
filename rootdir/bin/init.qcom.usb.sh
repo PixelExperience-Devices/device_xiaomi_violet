8c8caf0ce409707d7c813010452e4638fb8bf811
Sriharsha Allenki <sallenki@codeaurora.org>
2018-12-18 12:12:53 +0530

    violet: init.qcom.usb.sh: Add 90DB as default composition for trinket
    This change makes 90DB (interfaces: diag,dun,rmnet,dpl,qdss,adb)
    as default composition on trinket.
    
    Change-Id: I581e59cb9cf6787f90904f9dbbe6b41522ced60e

--- rootdir/bin/init.qcom.usb.sh
+++ rootdir/bin/init.qcom.usb.sh
@@ -129,7 +129,7 @@ if [ "$(getprop persist.vendor.usb.config)" == "" -a \
 	              "sdm845" | "sdm710")
 		          setprop persist.vendor.usb.config diag,serial_cdev,rmnet,dpl,adb
 		      ;;
-	              "msmnile" | "sm6150")
+	              "msmnile" | "sm6150" | "trinket")
 			  setprop persist.vendor.usb.config diag,serial_cdev,rmnet,dpl,qdss,adb
 		      ;;
 	              *)
