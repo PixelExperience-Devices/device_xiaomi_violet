c8d511f9048afd1db737d22db88d710a0301b76a
Vinay Gannevaram <vganneva@codeaurora.org>
2019-02-15 19:16:31 +0530

    sm6150-common: WIFIHAL: Support to send nl requests through wifihal Added support in wifihal to send nl messages on behalf of its clients. Since netadmin capabilities are given to wifihal, it is used as bridge to send nl messages to kernel Added support for clients to register for nl events.
    CRs-Fixed: 2424237
    Change-Id: If8b62733e1d298804e0be98918eb875fc52a4ad1

--- sm6150.mk
+++ sm6150.mk
@@ -372,6 +372,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
+    libwifi-hal-ctrl \
     libwifi-hal-qcom \
     libwpa_client \
     wpa_supplicant \
