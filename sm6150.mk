b64139c45c0fa0d18d6372239ed5953cdbf80c58
Cheney Ni <cheneyni@google.com>
2019-02-24 02:37:42 +0800

    sm6150-common: BluetoothAudio HAL: flip to be enabled by default
    Swap audio XML files, and supports following scenarios:
    1) (default) audio_policy_configuration.xml
       Using BluetoothAudio HAL and A2DP offload
    2) audio_policy_configuration_a2dp_offload_disabled.xml
       Using BluetoothAudio HAL only when A2DP offload is disabled
    3) audio_policy_configuration_bluetooth_legacy_hal.xml
       No BluetoothAudio HAL nor A2DP offload
    
    Bug: 126304229
    Test: Switch BluetoothAudio HAL and A2DP offload manully
    Change-Id: Id29dbb602cfeaf6c892a8c346811d576c4195038

--- sm6150.mk
+++ sm6150.mk
@@ -115,6 +115,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hearing_aid_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
