16175899b16480482435e59b96e45ba2daa28943
Ashay Jaiswal <ashayj@codeaurora.org>
2019-02-25 17:09:14 +0530

    violet: rootdir: Add script to update file permission
    Update file ownership of all the device files and power supplies
    exported by Charger/FG/QG/USBPD drivers to "system" user/group.
    
    Change-Id: I8149f20b8b48ac8f460e6b25b53225e1d9371c43

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -115,3 +115,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.safailnet.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qti.charger.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qti.charger.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
