91760653d20462df93caa91cdc1fafcb617153da
Naseer Ahmed <naseer@codeaurora.org>
2019-02-28 19:36:23 -0500

    sm6150-common: display: config: Add WCG props
    In recent builds, these configurations are now obtained via ro
    properties (reference: 126713799)
    
    Change-Id: I139701cf8890f00f4811b836ebfdc617b4b58606

--- common-props.mk
+++ common-props.mk
@@ -189,12 +189,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.has_HDR_display=true \
-    ro.surface_flinger.has_wide_color_display=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+
+# Graphics WCG properties
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696
 
 # IO CGroup
 PRODUCT_PROPERTY_OVERRIDES += \
