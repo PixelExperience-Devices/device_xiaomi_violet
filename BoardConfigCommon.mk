786591846723d2d10f67af8072a27e1affb9cdb7
Artem Borisov <dedsa2002@gmail.com>
2019-03-23 13:26:38 +0530

    sm6150-common: Enable full dexpreopt
    Change-Id: I61e71a20bbfc17998bd0c8bade7ee98faad15524

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -141,11 +141,12 @@ BOARD_USES_QCNE := true
 # Dex
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT ?= true
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
+      WITH_DEXPREOPT := true
+    endif
   endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 
 # DPM
 BOARD_USES_DPM := true
