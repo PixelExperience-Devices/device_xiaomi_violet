51ea9b575e8c5aa1b2aa7222a897ba50fe3dbaa6
Wei Wang <wvw@google.com>
2019-03-28 15:00:29 -0700

    sm6150-common: Rewrite display-helper into cpp
    Bug: 129495482
    Bug: 111098032
    Test: Boot and check PowerHAL
    Change-Id: I79b7276ff0c28885877af8b0533fe1f9d81b3b0c

--- power-libperfmgr/display-helper.cpp
+++ power-libperfmgr/display-helper.cpp
@@ -13,46 +13,38 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #define LOG_NIDEBUG 0
 #define LOG_TAG "android.hardware.power@1.3-service.sm6150-libperfmgr"
-
-#include <errno.h>
-#include <inttypes.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <dlfcn.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <inttypes.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
-
+#include <cutils/sockets.h>
 #include <log/log.h>
-
 #include "display-helper.h"
-
-int daemon_socket = -1;
-
+#define DAEMON_SOCKET "pps"
+static int daemon_socket = -1;
 static int connectPPDaemon() {
     // Setup socket connection, if not already done.
     if (daemon_socket < 0)
-        daemon_socket = socket_local_client(DAEMON_SOCKET,
-                                            ANDROID_SOCKET_NAMESPACE_RESERVED,
-                                            SOCK_STREAM);
-
-    if(daemon_socket < 0) {
+        daemon_socket =
+                socket_local_client(DAEMON_SOCKET, ANDROID_SOCKET_NAMESPACE_RESERVED, SOCK_STREAM);
+    if (daemon_socket < 0) {
         ALOGE("Connecting to socket failed: %s", strerror(errno));
         return -1;
     }
     return 0;
 }
-
-static int ppdComm(const char* cmd) {
+static int ppdComm(const char *cmd) {
     int ret = -1;
-
     ret = connectPPDaemon();
-    if (ret < 0) return ret;
-
+    if (ret < 0)
+        return ret;
     ret = write(daemon_socket, cmd, strlen(cmd));
     if (ret < 0) {
         ALOGE("Failed to send data over socket, %s", strerror(errno));
@@ -60,8 +52,6 @@ static int ppdComm(const char* cmd) {
     }
     return 0;
 }
-
-
 void set_display_lpm(int enable) {
     ALOGI("set_display_lpm state: %d", enable);
     if (enable) {
@@ -70,4 +60,3 @@ void set_display_lpm(int enable) {
         ppdComm("foss:off");
     }
 }
-
