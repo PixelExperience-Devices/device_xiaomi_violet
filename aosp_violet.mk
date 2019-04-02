f012e75dfbc97ff499818669f4dedb515076e08d
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-04-02 16:28:55 -0300

    violet: Use pixel fp to bypass safetynet

--- aosp_violet.mk
+++ aosp_violet.mk
@@ -26,10 +26,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "xiaomi/violet/violet:9/PKQ1.181203.001/V10.3.9.0.PFHINXM:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="violet-user 9 PKQ1.181203.001 V10.3.9.0.PFHINXM release-keys" \
     PRODUCT_NAME="violet" \
     TARGET_DEVICE="violet"
 
