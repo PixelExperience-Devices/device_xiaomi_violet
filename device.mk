dffeec703596d92dfb1faaa72b7ff9f6010f0c45
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-02 14:47:53 +0800

    violet: Import sm6150 light hal
    Change-Id: Ib50c16ba697a8a2d24e335a3ce155037a0172d68

--- device.mk
+++ device.mk
@@ -81,6 +81,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.xiaomi_sm6150
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     mokee.livedisplay@2.0-service-sdm
