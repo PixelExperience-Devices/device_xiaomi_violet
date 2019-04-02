799224a3472a603ddd3311a0fac9863296aa06ca
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-02 14:34:17 +0800

    sdm845-common: Drop sdm845 power hal
    Change-Id: Iebc52b2d6c637e4c7cf9499368fe8138cb93060e

--- sdm845.mk
+++ sdm845.mk
@@ -90,10 +90,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
-# Power
-PRODUCT_PACKAGES += \
-    power.qcom:64
-
 # QTI
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
