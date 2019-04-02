31a4d493cdb966adcf76bd558750d9a48ce4d0c2
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-02 14:33:11 +0800

    sdm845-common: Drop sdm845 LiveDisplay HAL
    Change-Id: Ieb3929bca0a1da2be97aa06b916a9aa0dc16716c

--- sdm845.mk
+++ sdm845.mk
@@ -80,8 +80,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    mokee.livedisplay@2.0-service-sdm \
-    mokee.livedisplay@2.0-service.xiaomi_sdm845
+    mokee.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
