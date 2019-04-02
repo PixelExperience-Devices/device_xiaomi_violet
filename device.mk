a217b7a3d6bb19c90d9d6a39dc3b1c7007d7f9d4
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-02 14:45:40 +0800

    violet: device: Include product_launched_with_p.mk
    Change-Id: I0fdff5883c11b492f1a17382844218f711ea2ead

--- device.mk
+++ device.mk
@@ -7,6 +7,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/violet/violet-vendor.mk)
