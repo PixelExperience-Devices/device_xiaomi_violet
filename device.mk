48f2615387165330769be04ad33e1fbffd05a114
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 18:30:36 +0800

    violet: device: Build libtinyxml2 from source
    Change-Id: Ib6e4d50d665ed70a0c66412f0755041b9f9ed459

--- device.mk
+++ device.mk
@@ -281,6 +281,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# Touchscreen
+PRODUCT_PACKAGES += \
+    libtinyxml2
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     mokee.trust@1.0-service
