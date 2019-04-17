51878086cb5d7483c2d300fb31e077d4dc70b4ab
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 15:43:05 +0800

    violet: audio: Copy audio config xml to vendor
    Change-Id: Ib4ac8ea2faa9dcd0d65df0e036fcbe3159cb2de4

--- device.mk
+++ device.mk
@@ -31,7 +31,7 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
