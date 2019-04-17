cea1c328e18b02addad412f1768c201146784b46
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 18:42:56 +0800

    violet: board: Import manifest.xml from stock
    Change-Id: Ic84418aaec6815baf7d47269185d8997e830be6a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,6 +114,7 @@ VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Media
