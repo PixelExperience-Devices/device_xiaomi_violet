46daac1f3edb964e9c2a34b02ae0969b74329cae
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 18:29:21 +0800

    violet: device: Build sensor hidl impl and service from source
    Change-Id: Id18155e0a124ae778fe82c9c8f4a819d2e4bcc00

--- device.mk
+++ device.mk
@@ -260,6 +260,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
