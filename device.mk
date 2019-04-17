7e896930c78792371eb584efa50cb2443b7b65a9
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 18:29:49 +0800

    violet: device: Build thermal hidl impl and service from source
    Change-Id: I0509d993d39fa73763dc5c548c3853295a7669dd

--- device.mk
+++ device.mk
@@ -276,6 +276,11 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     mokee.trust@1.0-service
