443db470f911151a81e2e6d9bbc97f361fc75b10
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 16:46:56 +0800

    violet: Config for camera
    Change-Id: Idf362da147470dd6ae633cc2498178975b6deb8e

--- device.mk
+++ device.mk
@@ -76,6 +76,10 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    libxml2 \
     Snap
 
 # Common init scripts
