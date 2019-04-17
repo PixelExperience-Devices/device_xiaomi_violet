29c91ff88df0e19bd06d615af6ffb99689bf75e8
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 16:59:29 +0800

    violet: Build FM packages
    Change-Id: I1ebe7b8ff685003def4ac2da86a56bdebaf19c6a

--- device.mk
+++ device.mk
@@ -124,6 +124,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# FM
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
