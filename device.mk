ce5d4116c21fd8121504b2ae1c742b34e13eb37f
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 19:24:21 +0800

    violet: device: Build power hidl impl and service from source
    Change-Id: Ifd972372ee00606ce05852fa92c9cf32be949d06

--- device.mk
+++ device.mk
@@ -248,6 +248,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
