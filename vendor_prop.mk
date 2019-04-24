02eb838bca07a8b648c15199bff05a47622dabeb
Pullakavi Srinivas <spullaka@codeaurora.org>
2019-04-24 18:27:34 +0530

    violet: Disable partial FB excl rect
    CRs-Fixed: 2336806
    Change-Id: I314ca09789055e42d1457ec81d04a1c569f2122f

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -142,6 +142,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_decimation=1 \
     vendor.display.enable_null_display=0 \
     vendor.display.disable_excl_rect=0 \
+    vendor.display.disable_excl_rect_partial_fb=1 \
     vendor.display.comp_mask=0 \
     vendor.display.disable_hw_recovery=0 \
     vendor.display.enable_default_color_mode=1 \
