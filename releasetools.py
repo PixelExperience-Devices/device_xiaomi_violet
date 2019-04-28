69549b88431b9f180e5eb10b31ab5e1bf415e2d1
XiNGRZ <chenxingyu92@gmail.com>
2019-04-29 03:23:26 +0800

    violet: releasetools: Add dtbo image to the output zip if present
    Change-Id: I8bb4da99945eceae3beab77489e6b9e2a47ab972

--- releasetools.py
+++ releasetools.py
@@ -18,12 +18,34 @@ import hashlib
 import common
 import re
 
+def FullOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+  return
+
+def IncrementalOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+  return
+
 def FullOTA_Assertions(info):
   AddBasebandAssertion(info, info.input_zip)
   return
 
 def IncrementalOTA_Assertions(info):
   AddBasebandAssertion(info, info.target_zip)
+  return
+
+def AddImage(info, basename, dest):
+  path = "IMAGES/" + basename
+  if path not in info.input_zip.namelist():
+    return
+
+  data = info.input_zip.read(path)
+  common.ZipWriteStr(info.output_zip, basename, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+
+def OTA_InstallEnd(info):
+  info.script.Print("Patching firmware images...")
+  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   return
 
 def AddBasebandAssertion(info, input_zip):
