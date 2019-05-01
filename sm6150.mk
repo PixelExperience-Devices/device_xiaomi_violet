f9d5695016d16b3d2dc86254d17e13436c69bd75
Ananth Raghavan Subramanian <sananth@codeaurora.org>
2019-05-01 13:50:53 -0700

    sm6150-common: power: Add config
    Added product and board makefiles.
    Moved powerhint XMLs to this project.
    
    Change-Id: I3d36f29f69f09166f84d2e42a6480cb590b04d2d

--- sm6150.mk
+++ sm6150.mk
@@ -266,7 +266,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service.xiaomi_sm6150
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
+    $(LOCAL_PATH)/power/config/msmsteppe/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # QTI
