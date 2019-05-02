9c3691cb9de5c2f42e015f7bda278210b7df1592
GuaiYiHu <guaiyihu@foxmail.com>
2019-05-02 15:04:19 +0800

    violet: Build qti power hal
    Change-Id: I107602d906787dfd5c4e3b9afaac1a9b17a8f147

--- device.mk
+++ device.mk
@@ -261,8 +261,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power@1.1-service-qti
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
