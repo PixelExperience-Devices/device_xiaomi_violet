0b0e7a838e589087a6ce7dd991d0c562263d9c6b
GuaiYiHu <guaiyihu@foxmail.com>
2019-05-02 15:02:56 +0800

    violet: Ship prebuilt vbmeta image
    Change-Id: I6b38f5113a1a6c49a1ca708afc8a23abf7240dec

--- releasetools.py
+++ releasetools.py
@@ -46,6 +46,7 @@ def AddImage(info, basename, dest):
 def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   return
 
 def AddBasebandAssertion(info, input_zip):
