05bbb487deb5b1136c60f3c26b32a5f9054f03ac
GuaiYiHu <guaiyihu@foxmail.com>
2019-05-02 15:09:23 +0800

    Revert "sdm845-common: Add QCOM's WFD implementation"
     * Virtual display is broken on our device.
     * This temp fixes screen recording.
    
    This reverts commit 807d0b3b87428e76ec8b5b047d8a7086e6207686.
    
    Change-Id: Iee2435b9f4d69a6ba594009aafc97f503746649b

--- device.mk
+++ device.mk
@@ -145,8 +145,7 @@ PRODUCT_PACKAGES += \
     libdisplayconfig \
     libqdMetaData.system \
     libtinyxml \
-    libvulkan \
-    vendor.display.config@1.0
+    libvulkan
 
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.1-service \
@@ -378,10 +377,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libnl
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
