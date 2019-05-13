7cfdbf9e7a02033840888ff001cd884cbab1b50e
GuaiYiHu <guaiyihu@foxmail.com>
2019-05-13 22:12:44 +0800

    violet: keylayout: Add button mapping for headphone control
     * Xiaomi handles this on framework side but we don't, so add this to make headphone control work.
    
    Change-Id: I7160189ded1261a8427cf59417c86c7049c5c832

--- device.mk
+++ device.mk
@@ -278,6 +278,7 @@ PRODUCT_COPY_FILES += \
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/sm6150-idp-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/sm6150-idp-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
