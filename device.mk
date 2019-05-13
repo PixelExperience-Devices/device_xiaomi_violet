fc433eb4d4b21ae10d0fde47cbb673fd4475755f
Wang Han <416810799@qq.com>
2019-05-13 20:33:41 +0800

    violet: Set AAPT configuration
    Change-Id: I0aa83727943ac59069f1d27496a2a1b5855cf607

--- device.mk
+++ device.mk
@@ -22,6 +22,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
