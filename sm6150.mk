54097d5ce0f43bf48028c835bae176d9a8ad9e69
GuaiYiHu <guaiyihu@foxmail.com>
2019-05-16 23:04:58 +0800

    sm6150-common: Build thermal hal from source
    Change-Id: I646f18b30284e5ee6dcf62d36199f03c9150423b

--- sm6150.mk
+++ sm6150.mk
@@ -349,7 +349,8 @@ PRODUCT_BOOT_JARS += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    thermal.sm6150
 
 # TextClassifier
 PRODUCT_PACKAGES += \
