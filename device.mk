081d0462ed62d2606b61f3ae87aff641aef61038
merothh <merothh@gmail.com>
2019-05-27 00:03:09 +0530

    violet: Build ThermalController

--- device.mk
+++ device.mk
@@ -344,6 +344,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.sm6150
 
+# ThermalController app
+PRODUCT_PACKAGES += \
+    ThermalController
+
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
