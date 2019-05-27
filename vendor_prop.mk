6563560b759fa01b4471136d054415218864d907
Dyneteve <sound0020@gmail.com>
2019-05-27 15:45:08 +0300

    violet: Rewrite device props into vendor_prop.mk

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,83 +1,133 @@
+# Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.job_delay=true \
-    persist.sys.mcd_config_file=/system/etc/mcd_default.conf \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m \
-    ro.hardware.keystore_desede=true \
-    persist.radio.multisim.config=dsds \
-    persist.vendor.qcomsysd.enabled=1 \
-    ro.vendor.extension_library=libqti-perfd-client.so \
-    persist.vendor.radio.apm_sim_not_pwdn=1 \
-    persist.vendor.radio.sib16_support=1 \
-    persist.vendor.radio.custom_ecc=1 \
-    persist.vendor.radio.rat_on=combine \
-    persist.backup.ntpServer=0.pool.ntp.org \
-    sys.vendor.shutdown.waittime=500 \
-    ro.frp.pst=/dev/block/bootdevice/by-name/frp \
-    persist.demo.hdmirotationlock=false \
-    persist.sys.sf.color_saturation=1.0 \
-    debug.sf.hw=0 \
-    debug.egl.hw=0 \
-    debug.sf.latch_unsignaled=1 \
-    debug.mdpcomp.logs=0 \
-    ro.vendor.display.cabl=0 \
-    vendor.gralloc.disable_ubwc=0 \
-    vendor.display.disable_scaler=0 \
-    vendor.display.disable_inline_rotator=1 \
-    vendor.display.disable_decimation=1 \
-    vendor.display.disable_excl_rect_partial_fb=1 \
-    vendor.display.enable_null_display=0 \
-    vendor.display.disable_excl_rect=0 \
-    vendor.display.comp_mask=0 \
-    vendor.display.disable_hw_recovery=1 \
-    vendor.display.enable_default_color_mode=0 \
-    vendor.display.dataspace_saturation_matrix=1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0 \
     af.fast_track_multiplier=1 \
-    vendor.audio_hal.period_size=192 \
-    ro.vendor.audio.sdk.fluencetype=none \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicerec=false \
     persist.vendor.audio.fluence.speaker=true \
-    vendor.audio.tunnel.encode=false \
     persist.vendor.audio.ras.enabled=false \
-    vendor.audio.offload.buffer.size.kb=32 \
-    audio.offload.min.duration.secs=30 \
-    audio.offload.video=true \
+    ro.vendor.audio.sdk.fluencetype=none \
+    ro.vendor.audio.sdk.ssr=false \
+    ro.vendor.audio.sos=true \
+    ro.config.media_vol_steps=25 \
+    ro.config.vc_call_vol_steps=7 \
+    vendor.audio.tunnel.encode=false \
     vendor.audio.offload.track.enable=false \
-    audio.deep_buffer.media=true \
-    vendor.voice.path.for.pcm.voip=true \
     vendor.audio.offload.multiaac.enable=true \
     vendor.audio.dolby.ds2.enabled=false \
     vendor.audio.dolby.ds2.hardbypass=false \
     vendor.audio.offload.multiple.enabled=false \
     vendor.audio.offload.passthrough=false \
-    ro.vendor.audio.sdk.ssr=false \
     vendor.audio.offload.gapless.enabled=true \
     vendor.audio.safx.pbe.enabled=true \
-    vendor.audio.parser.ip.buffer.size=262144 \
     vendor.audio.flac.sw.decoder.24bit=true \
-    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac \
-    ro.vendor.audio.sos=true \
-    ro.vendor.audio.voice.volume.boost=manual \
     vendor.audio.use.sw.alac.decoder=true \
     vendor.audio.use.sw.ape.decoder=true \
     vendor.audio.hw.aac.encoder=true \
+    vendor.audio_hal.period_size=192 \
+    vendor.audio.offload.buffer.size.kb=32 \
+    vendor.audio.parser.ip.buffer.size=262144 \
     vendor.audio.noisy.broadcast.delay=600 \
     vendor.audio.offload.pstimeout.secs=3 \
-    ro.af.client_heap_size_kbyte=7168 \
     vendor.audio_hal.in_period_size=144 \
     vendor.audio_hal.period_multiplier=3 \
-    vendor.audio.adm.buffering.ms=2 \
-    ro.wlan.chip=39xx \
-    ro.wlan.mimo=1 \
-    ro.opengles.version=196610 \
+    vendor.audio.adm.buffering.ms=2
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
     vendor.qcom.bluetooth.soc=cherokee \
-    camera.disable_zsl_mode=1 \
-    ro.vendor.at_library=libqti-at.so \
-    persist.vendor.qti.games.gt.prof=1 \
-    ro.vendor.iocgrp.config=1 \
-    ro.crypto.volume.filenames_mode=aes-256-cts \
-    ro.crypto.allow_encrypt_override=true
+    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac
+
+# FRP
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/bootdevice/by-name/frp
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.atfwd.start=true \
+    persist.vendor.radio.data_con_rprt=1 \
+    persist.vendor.radio.force_on_dc=true \
+    persist.vendor.radio.snapshot_enabled=1 \
+    persist.vendor.radio.snapshot_timer=5 \
+    persist.vendor.radio.redir_party_num=1 \
+    persist.vendor.radio.report_codec=1 \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.sib16_support=1 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.radio.multisim.config=dsds \
+    ril.subscription.types=NV,RUIM \
+    telephony.lteOnCdmaDevice=1 \
+    ro.telephony.default_network=22,22
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.mm.enable.qcom_parser=63963135 \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-qcp=true \
+    media.stagefright.enable-fma2dp=true \
+    media.stagefright.enable-scan=true \
+    media.aac_51_output_enabled=true \
+    persist.mm.enable.prefetch=true \
+    mmp.enable.3g2=true \
+    mm.enable.smoothstreaming=true
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    vendor.iop.enable_uxe=0 \
+    vendor.perf.iop_v3.enable=true \
+    vendor.perf.gestureflingboost.enable=true \
+    vendor.perf.workloadclassifier.enable=true
+
+# CNE and DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.cne.feature=1 \
+    persist.vendor.dpm.feature=1
+
+# ZRAM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.config.zram=true
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+
+# Netmgr
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.df.dev_name=rmnet_usb0 \
+    persist.vendor.data.iwlan.enable=true \
+    persist.vendor.data.mode=concurrent
+
+# OpenGLES
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196610
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.hw=0 \
+    debug.egl.hw=0 \
+    debug.sf.latch_unsignaled=1 \
+    ro.vendor.display.cabl=0 \
+    ro.vendor.display.sensortype=2 \
+    vendor.gralloc.disable_ubwc=0 \
+    vendor.display.disable_scaler=0 \
+    vendor.display.disable_inline_rotator=1 \
+    vendor.display.disable_decimation=1 \
+    vendor.display.enable_null_display=0 \
+    vendor.display.disable_excl_rect=0 \
+    vendor.display.comp_mask=0 \
+    vendor.display.disable_hw_recovery=0 \
+    vendor.display.enable_default_color_mode=0 \
+    vendor.display.disable_hw_recovery_dump=1 \
+    vendor.display.dataspace_saturation_matrix=1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0
