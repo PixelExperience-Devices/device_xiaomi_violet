fdc1e459c1322d0bc1a1283b9be4f5c42c5095ff
merothh <merothh@gmail.com>
2019-05-27 19:59:29 +0530

    violet: Drop unneeded LineageOS stuff

--- device.mk
+++ device.mk
@@ -352,10 +352,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
