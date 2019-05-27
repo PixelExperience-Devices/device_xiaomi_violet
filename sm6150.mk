71f5a6d05c5aab7870a4d618da2b5ae2b5d59bab
Ramakant Singh <ramakant@codeaurora.org>
2019-05-27 15:27:26 +0530

    sm6150-common: display/config: Add libdisplayconfig in PRODUCT_PACKAGES
    Change-Id: I3775fee080ea8a6a57ac4f9306bbe4a8d57b5add

--- sm6150.mk
+++ sm6150.mk
@@ -407,7 +407,8 @@ PRODUCT_COPY_FILES += \
 # WiFi Display
 PRODUCT_PACKAGES += \
     libln \
-    libqdMetaData.system
+    libqdMetaData.system \
+    libdisplayconfig.vendor
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
