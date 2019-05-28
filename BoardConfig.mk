025338ff859092e5392d21e6c691e011fa76cbeb
Felix <google@ix5.org>
2019-05-28 13:42:39 +0200

    violet: Makefile: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated
    
    Change-Id: Ia66f8462d7b7c546d8bd4d25c6fdfd22b1726841
    Signed-off-by: Felix <google@ix5.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,7 +195,7 @@ TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_SEPOLICY_M4DEFS += \
     debugfs_clk=vendor_debugfs_clk \
     debugfs_ipc=vendor_debugfs_ipc \
