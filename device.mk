ea9c6857cbf6a3493c1b26dd686850fc450a87ff
Dyneteve <sound0020@gmail.com>
2019-06-19 12:34:47 +0300

    violet: Build qti camera device

--- device.mk
+++ device.mk
@@ -119,6 +119,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     libxml2 \
     Snap
 
