0a7a71acbf4f36209934399232959cb1763ee2f6
Dhananjay Kumar <dhakumar@codeaurora.org>
2019-06-26 19:00:01 +0530

    sm6150-common: enable Audio HIDL HAL 5.0
    Change-Id: I1d9ea3cb6bdf99e42419f1b82bd87fa64692c9b5

--- sm6150.mk
+++ sm6150.mk
@@ -78,16 +78,10 @@ PRODUCT_PACKAGES += \
     fs_config_files
 
 # Audio
-  PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio@4.0 \
-    android.hardware.audio.common@4.0 \
-    android.hardware.audio.common@4.0-util \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0 \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.1-impl:32 \
     audio.a2dp.default \
     audio.r_submix.default \
