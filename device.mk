b00a70ecf8b0a7ed574a43519383329beab82285
AnilKumar Chimata <anilc@codeaurora.org>
2019-06-27 16:41:38 +0530

    violet: Remove keymaster enablement flags
    Remove keymaster enablement flags and corresponding
    entries from .mk files because these flags are moved
    to respective modules.

--- device.mk
+++ device.mk
@@ -330,7 +330,6 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
-    init.qti.qseecomd.sh \
     init.target.rc \
     ueventd.qcom.rc \
     fstab.qcom \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -52,14 +52,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qti.qseecomd.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.qti.qseecomd.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.usb.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
