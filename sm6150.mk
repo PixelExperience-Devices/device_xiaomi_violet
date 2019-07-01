29637e24e87a1e3882e01e32975733307ee2b5fd
Demon000 <demonsingur@gmail.com>
2019-07-02 01:32:47 +0200

    sm6150-common: Implement in-screen fingerprint HAL
    Change-Id: I8fd985bf4eab1747351f9e717cc33fa6d5ab1517

--- sm6150.mk
+++ sm6150.mk
@@ -171,6 +171,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+ifeq ($(strip $(TARGET_USES_FOD)),true)
+PRODUCT_PACKAGES += \
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_sm6150
+endif
+
  # FM
 PRODUCT_PACKAGES += \
     FM2 \
