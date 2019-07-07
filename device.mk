50ced0fe7c66618a3c62aaa7a39e4d97005519af
Wang Han <416810799@qq.com>
2019-07-07 16:20:07 +0800

    violet: Remove redundant libxml2
     * This is accidentally added twice.
    
    Change-Id: I7621138bac8b601a614b24f7b1692a23c2244e15

--- device.mk
+++ device.mk
@@ -124,7 +124,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    libxml2 \
     Snap
 
 # Context Hub
