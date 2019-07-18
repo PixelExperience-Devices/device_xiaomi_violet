51cf7c291c091cd23a77df63d60e9fcbd5425829
LuK1337 <priv.luk@gmail.com>
2019-07-18 19:41:39 +0200

    violet: Set proper fingerprint sensor location
    Change-Id: I3cfc4d198c3a88724e3a268a4e9e9b7ab9909f9b
    Signed-off-by: Hasaber8(Rohan Hasabe) <rohanhasabe8@gmail.com>

--- device.mk
+++ device.mk
@@ -15,7 +15,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Inherit from sm6150-common
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
