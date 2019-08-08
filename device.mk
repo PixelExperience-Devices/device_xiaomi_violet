6c97c798d4f664d612853155c3ac02fad2bff103
GuaiYiHu <guaiyihu@foxmail.com>
2019-08-08 14:32:12 +0800

    violet: Move powerhint.xml from vendor tree to device tree
    Change-Id: I33159c070a3f83ee5a2b908c60b18d5216e8f49f

--- device.mk
+++ device.mk
@@ -290,7 +290,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
