a17f903dc477c020c6a8a665d5789df42392427a
PIPIPIG233666 <pig.priv@gmail.com>
2019-08-08 07:44:33 -0400

    sm6150-common: Kill init.qti.fm.sh
    Change-Id: Ie274ba2a230ec06adc5f796ca0957a9eabd09f06

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -82,14 +82,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qti.fm.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.qti.fm.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional eng
