48d80eeb6764b7ffd7af1568b1d47ec3aa5dadc8
merothh <merothh@gmail.com>
2019-08-25 21:38:38 +0530

    violet: Drop Dirac
    * Dirac is no longer allowed on Pie Edition
    * Keep blob listing so that we dont have to split the vendor tree into two
    branches for Pie Plus edition
    
    Revert "violet: Add Initial support for dirac"
    
    This reverts commit 16d79d6998e51dc8dffa8a32a0877fd6f0d9e3a8.
    
    Revert "violet: dirac: Prevent failures when toggling"
    
    This reverts commit 8975c1d2f7899cca0efa8384ce13aeb5d5df9d44.
    
    Revert "violet: dirac: Only call setEnabled on boot completed"
    
    This reverts commit 9f20ea43e63099806b158faa720bcf49c38c82b4.
    
    Revert "violet: Refactor Dirac setup"
    
    This reverts commit 94ee9449e694e77329d8ac6bfadd255fa1c7db22.
    
    Revert "violet: Dirac fixes"
    
    This reverts commit 212d42122021f0887ba07d9692fcfa12679b7020.
    
    Revert "violet: parts: Import dirac translations from MIUI"
    
    This reverts commit ee2ace78d6b030f1022452cd638cfe35a9383180.
    
    Revert "violet: parts: Refactor dirac strings"
    
    This reverts commit 15e8ebce0fcc713c8422fd2294ca9a5d5da0e0aa.
    
    Revert "violet: parts: Add summary for dirac"
    
    This reverts commit 1dff95725cd757ec9634ee4bb58e2e04452b0624.
    
    Revert "violet: parts: Add DiracSound backend"
    
    This reverts commit 29084e4584f8e428dfe0da1f2e94123de5df4fce.
    
    Revert "violet: parts: Introduce Dirac preferences"
    
    This reverts commit 508ef8877eb241597887b56d98694a118a28eca2.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,7 +22,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -36,6 +35,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             if (DEBUG) Log.d(TAG, "Starting Doze service");
             DozeUtils.startService(context);
         }
-        new DiracUtils(context).onBootCompleted();
     }
+
 }
