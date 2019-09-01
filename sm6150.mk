e6de54674e9e0cd1c181dd908b3966d12bfc29a0
PIPIPIG233666 <pig.priv@gmail.com>
2019-08-31 20:37:09 -0400

    sm6150-common: Switch back to custom power HAL
    This reverts commit 317d82ed2572005731f26ca0fc5c378770fe38a1.
    
    Change-Id: I289667c7388f20bf6da27ff4819239abcf8e8d29

--- sm6150.mk
+++ sm6150.mk
@@ -273,7 +273,9 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service \
+    power.qcom:64
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
