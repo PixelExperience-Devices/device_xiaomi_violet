ab9996ae2dccfec2b5903c39be113207c6f34eaf
TheScarastic <warabhishek@gmail.com>
2019-09-14 23:31:49 +0530

    sm6150-common: Add support for camera popup light
    Change-Id: I90fe65e80b33f6aa6250d2c9701f0705f09aa01d

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
@@ -26,12 +26,14 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.IBinder;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.util.Log;
 
 import java.util.List;
 
+import org.lineageos.internal.util.FileUtils;
 import vendor.xiaomi.hardware.motor.V1_0.IMotor;
 
 public class PopupCameraService extends Service {
@@ -47,6 +49,11 @@ public class PopupCameraService extends Service {
     private SensorManager mSensorManager;
     private Sensor mFreeFallSensor;
     private static final int FREE_FALL_SENSOR_ID = 33171042;
+
+    private static final String GREEN_LED_PATH = "/sys/class/leds/green/brightness";
+    private static final String BLUE_LED_PATH = "/sys/class/leds/blue/brightness";
+
+    private static Handler mHandler = new Handler();
 
     @Override
     public void onCreate() {
@@ -105,14 +112,29 @@ public class PopupCameraService extends Service {
             return;
         try {
             if (cameraState.equals(openCameraState) && mMotor.getMotorStatus() == 13) {
+                lightUp();
                 mMotor.popupMotor(1);
                 mSensorManager.registerListener(mFreeFallListener, mFreeFallSensor, SensorManager.SENSOR_DELAY_NORMAL);
             } else if (cameraState.equals(closeCameraState) && mMotor.getMotorStatus() == 11) {
+                lightUp();
                 mMotor.takebackMotor(1);
                 mSensorManager.unregisterListener(mFreeFallListener, mFreeFallSensor);
             }
         } catch (Exception e) {
         }
+    }
+
+    private void lightUp() {
+        FileUtils.writeLine(GREEN_LED_PATH, "255");
+        FileUtils.writeLine(BLUE_LED_PATH, "255");
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FileUtils.writeLine(GREEN_LED_PATH, "0");
+                FileUtils.writeLine(BLUE_LED_PATH, "0");
+            }
+        }, 1200);
     }
 
     private SensorEventListener mFreeFallListener = new SensorEventListener() {
