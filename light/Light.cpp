7b4f4694580b123e10cea4ff021c65e5e2fd6fb2
PIPIPIG233666 <pig.priv@gmail.com>
2019-09-20 17:52:50 -0400

    sm6150-common: light: Add real blue notification light for davinci
     * Davinci has two lights, each on one side,
       in 32ae4fa411cc4c733862dab7bbc3e29717fe5ce9, I only added support for green LED.
     * In this commit, I make blue one works as well to make it look better.
    
    Change-Id: I4eecc8c3a2284249cd1b5b14c4fa66b7e63d859c

--- light/Light.cpp
+++ light/Light.cpp
@@ -24,7 +24,8 @@
 
 #define LCD_LED         "/sys/class/backlight/panel0-backlight/"
 #define WHITE_LED       "/sys/class/leds/red/"
-#define BLUE_LED        "/sys/class/leds/green/"
+#define BLUE_LED        "/sys/class/leds/blue/"
+#define GREEN_LED       "/sys/class/leds/green/"
 
 #define BREATH          "breath"
 #define BRIGHTNESS      "brightness"
@@ -109,24 +110,30 @@ static void handleBacklight(const LightState& state) {
 static void handleNotification(const LightState& state) {
     uint32_t whiteBrightness = getScaledBrightness(state, getMaxBrightness(WHITE_LED MAX_BRIGHTNESS));
     uint32_t blueBrightness = getScaledBrightness(state, getMaxBrightness(BLUE_LED MAX_BRIGHTNESS));
+    uint32_t greenBrightness = getScaledBrightness(state, getMaxBrightness(GREEN_LED MAX_BRIGHTNESS));
 
     /* Disable breathing */
     set(WHITE_LED BREATH, 0);
     set(BLUE_LED BREATH, 0);
+    set(GREEN_LED BREATH, 0);
 
     if (state.flashMode == Flash::TIMED) {
-        /* White and Blue */
+        /* White, Blue and GREEN */
         set(WHITE_LED DELAY_OFF, state.flashOnMs);
         set(WHITE_LED DELAY_ON, state.flashOffMs);
         set(BLUE_LED DELAY_OFF, state.flashOnMs);
 	set(BLUE_LED DELAY_ON, state.flashOffMs);
+        set(GREEN_LED DELAY_OFF, state.flashOnMs);
+	set(GREEN_LED DELAY_ON, state.flashOffMs);
 
         /* Enable Breathing */
         set(WHITE_LED BREATH, 1);
         set(BLUE_LED BREATH, 1);
+        set(BLUE_LED BREATH, 1);
     } else {
         set(WHITE_LED BRIGHTNESS, whiteBrightness);
         set(BLUE_LED BRIGHTNESS, blueBrightness);
+        set(GREEN_LED BRIGHTNESS, greenBrightness);
     }
 }
 
