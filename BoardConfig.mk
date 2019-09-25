36ee62258be4f03caec93ca6ce1f619899765aef
Bruno Martins <bgcngm@gmail.com>
2019-09-25 23:00:35 +0100

    violet: Kill framework compatibility matrix with fire
    This thing is not mandatory at all and only demands extra work by having
    to keep it up-to-date whenever new interfaces are added to the device manifest
    or simply uprev'ed.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,7 +128,6 @@ TARGET_USES_HWC2 := true
 TARGET_USES_ION := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
