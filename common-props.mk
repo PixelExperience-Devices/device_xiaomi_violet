667e4fb7c94058e29c94ef413dd93f85ab170c84
LuK1337 <priv.luk@gmail.com>
2019-09-25 18:42:24 +0200

    sm6150-common: Convert remaining configstore settings to props
    Change-Id: Iadefa31bc863b7ab70239874bed82bef240cbba5

--- common-props.mk
+++ common-props.mk
@@ -124,6 +124,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
