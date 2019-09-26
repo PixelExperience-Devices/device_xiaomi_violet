bd304fbe709c71ce78cd97f92ccd71fb18c36dc6
Tharaga Balachandran <tbalacha@codeaurora.org>
2019-09-26 19:17:58 -0400

    violet: gralloc: Upgrade mapper and allocators to v3.0
    CRs-Fixed: 2445393
    Change-Id: I8c44ab63ca81523bb6acb75e8ae878cc7466e139

--- device.mk
+++ device.mk
@@ -179,7 +179,7 @@ PRODUCT_PACKAGES += \
 # Display/Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
-    android.hardware.graphics.mapper@2.0-impl-qti-display \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     gralloc.sm6150 \
@@ -194,7 +194,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer@1.0.vendor \
     vendor.qti.hardware.display.composer@2.0.vendor \
-    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
