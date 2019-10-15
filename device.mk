61d69ced3cc28ced76516b5317322adec2aff12a
Dyneteve <sound0020@gmail.com>
2019-10-15 22:04:36 +0300

    violet: Build atrace hal

--- device.mk
+++ device.mk
@@ -122,6 +122,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
