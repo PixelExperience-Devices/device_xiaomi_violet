272fb3e9459d8002a7ec5698874f13aac1a4f63e
Dyneteve <sound0020@gmail.com>
2019-10-16 15:19:51 +0300

    violet: Uprev wifi supplicant to 2.1

--- device.mk
+++ device.mk
@@ -409,7 +409,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf \
     vendor.qti.hardware.wifi.hostapd@1.1.vendor \
-    vendor.qti.hardware.wifi.supplicant@2.0.vendor
+    vendor.qti.hardware.wifi.supplicant@2.1.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
