d54d0191ff97821736555b83e4b626af56d49066
Dyneteve <sound0020@gmail.com>
2019-10-23 18:00:50 +0300

    violet: Misc cleanup
    * Nuke opus audio decoder since c2 is disabled
    * Update gps HAL from davinci
    * Update gps configs from davinci
    * Nuke OSS gps hal
    * Sort proprietary-files

--- device.mk
+++ device.mk
@@ -214,12 +214,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.1-impl-qti \
-    libgnss \
-    libgnsspps \
-    libsensorndkbridge
-
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service
@@ -369,7 +363,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
 
 # Telephony
 PRODUCT_PACKAGES += \
--- gps/android/Android.mk
+++
@@ -1,9 +0,0 @@
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-LOCAL_PATH := $(call my-dir)
-ifeq ($(GNSS_HIDL_VERSION),1.1)
-include $(LOCAL_PATH)/1.1/Android.mk
-else
-include $(LOCAL_PATH)/1.0/Android.mk
-endif
-
-endif #BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
