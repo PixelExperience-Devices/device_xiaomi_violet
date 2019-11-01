6c0d32d6600314ff839dfcb9e0bcb526bce2e4c2
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    sm6150-common: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- sm6150.mk
+++ sm6150.mk
@@ -21,6 +21,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Properties
 -include $(LOCAL_PATH)/common-props.mk
