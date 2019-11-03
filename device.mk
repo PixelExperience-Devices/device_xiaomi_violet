7a975d36cae32f4e0919c8ff69e81b959264fd88
Dyneteve <sound0020@gmail.com>
2019-11-03 13:15:01 +0200

    Revert "Revert "Revert "violet: Introduce Safailnet"""
    * Also switch to enforcing
    
    This reverts commit 9363bc5c092470200362f2be9ec130d7ef52ef90.

--- device.mk
+++ device.mk
@@ -344,8 +344,7 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.target.rc \
     ueventd.qcom.rc \
-    fstab.qcom \
-    init.safailnet.rc
+    fstab.qcom
 
 # RCS
 PRODUCT_PACKAGES += \
