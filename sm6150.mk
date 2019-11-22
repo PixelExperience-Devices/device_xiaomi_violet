c7e845bb8f87400c0b06d5fc8ac43f7b28fe9f26
LuK1337 <priv.luk@gmail.com>
2019-11-22 15:35:26 +0100

    sm6150-common: Exclude Snap overlays from RRO
    Change-Id: Ibefb7acaa158eea787618d1696057b75d2541e87

--- sm6150.mk
+++ sm6150.mk
@@ -21,7 +21,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # Properties
 -include $(LOCAL_PATH)/common-props.mk
