9a42b0fca9d521a991e907757a9d07a16b090354
Dyneteve <32036579+Dyneteve@users.noreply.github.com>
2019-11-24 14:05:17 +0200

    violet: Nuke custom sf offsets

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -160,9 +160,3 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_app_phase_offset_ns=1500000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_phase_offset_ns=1500000
