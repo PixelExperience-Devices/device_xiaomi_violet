fa8ca78b58320e6d56acdd45ef0477eb7cd778d1
Dyneteve <sound0020@gmail.com>
2019-11-25 19:35:24 +0200

    violet: Uprev mapper and allocator to 3.0

--- device.mk
+++ device.mk
@@ -159,7 +159,7 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
-    android.hardware.graphics.mapper@2.0-impl-qti-display \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     gralloc.sm6150 \
