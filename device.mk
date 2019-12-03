c733a42e54741dd1fd0cb85cc474569cee3f6452
Dyneteve <sound0020@gmail.com>
2019-12-03 15:32:06 +0200

    violet: Import init.recovery.qcom

--- device.mk
+++ device.mk
@@ -333,6 +333,7 @@ PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
+    init.recovery.qcom.rc \
     init.qcom.sensors.sh \
     init.qti.chg_policy.sh \
     init.qcom.sh \
