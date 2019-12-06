5d162a22cf0642f0fc3f68f3e2dd146265d8d291
TheScarastic <warabhishek@gmail.com>
2019-12-06 16:44:18 +0530

    violet: parts: Refactor whole code
    Change-Id: I14303556c66ea6bd3d111b5ce7e176711a676824

--- parts/src/org/lineageos/settings/doze/DozeService.java
+++ parts/src/org/lineageos/settings/doze/DozeService.java
@@ -34,6 +34,16 @@ public class DozeService extends Service {
 
     private ProximitySensor mProximitySensor;
     private PickupSensor mPickupSensor;
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                onDisplayOn();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                onDisplayOff();
+            }
+        }
+    };
 
     @Override
     public void onCreate() {
@@ -88,15 +98,4 @@ public class DozeService extends Service {
             mProximitySensor.enable();
         }
     }
-
-    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
-                onDisplayOn();
-            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
-                onDisplayOff();
-            }
-        }
-    };
 }
