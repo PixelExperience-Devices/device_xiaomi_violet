da488f64118ffed5cb0d6c3678ef9c92ca3da1eb
PIPIPIG233666 <pig.priv@gmail.com>
2019-12-07 21:16:55 -0500

    sm6150-common: Update blobs
     * Drop unused blobs.
     * From LA.UM.8.9.r1-05500-SM6xx.0
    
    Change-Id: I4093c7086dad7587e074aaec405324c94643c039

--- sm6150.mk
+++ sm6150.mk
@@ -319,10 +319,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
-# MSM IRQ
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # Native Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
