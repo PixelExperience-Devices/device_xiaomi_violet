511499c8db677e8e149388c841d2e5efb0c3054b
Dyneteve <dyneteve@pixelexperience.org>
2019-12-23 15:55:28 +0200

    violet: Include GoogleCamera by default

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/violet/violet-vendor.mk)
 
+# GoogleCamera
+$(call inherit-product-if-exists, packages/apps/GoogleCamera/gcam.mk)
+
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
