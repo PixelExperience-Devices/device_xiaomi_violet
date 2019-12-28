b060b01ebac4455b691c58b2cf8051a7a2044512
PIPIPIG233666 <pig.priv@gmail.com>
2019-12-27 20:33:55 -0500

    sm6150-common: Build vendor.qti.hardware.btconfigstore@1.0 from source
    Change-Id: Id3fdd9dc8df011cb5652ab11b2ab9778dcf5c80a

--- sm6150.mk
+++ sm6150.mk
@@ -138,7 +138,8 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbthost_if \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
