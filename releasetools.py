f265ede49286f2ba7e513b393f495b735c1f55b2
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-12-30 12:27:57 -0300

    violet: releasetools: Fix incremental ota gen

--- releasetools.py
+++ releasetools.py
@@ -19,38 +19,46 @@ import common
 import re
 
 def FullOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
+  OTA_InstallEnd(info, False)
   return
 
 def IncrementalOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
+  OTA_InstallEnd(info, True)
   return
 
 def FullOTA_Assertions(info):
-  AddBasebandAssertion(info)
+  AddBasebandAssertion(info, False)
   return
 
 def IncrementalOTA_Assertions(info):
-  AddBasebandAssertion(info)
+  AddBasebandAssertion(info, True)
   return
 
-def AddImage(info, basename, dest):
+def AddImage(info, basename, dest, incremental):
+  if incremental:
+    input_zip = info.source_zip
+  else:
+    input_zip = info.input_zip
   path = "IMAGES/" + basename
-  if path not in info.input_zip.namelist():
+  if path not in input_zip.namelist():
     return
 
-  data = info.input_zip.read(path)
+  data = input_zip.read("IMAGES/" + basename)
   common.ZipWriteStr(info.output_zip, basename, data)
   info.script.Print("Flashing {} image".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
-def OTA_InstallEnd(info):
-  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
-  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+def OTA_InstallEnd(info, incremental):
+  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo", incremental)
+  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta", incremental)
   return
 
-def AddBasebandAssertion(info):
-  android_info = info.input_zip.read("OTA/android-info.txt")
+def AddBasebandAssertion(info, incremental):
+  if incremental:
+    input_zip = info.source_zip
+  else:
+    input_zip = info.input_zip
+  android_info = input_zip.read("OTA/android-info.txt")
   m = re.search(r'require\s+version-baseband\s*=\s*(.+)', android_info)
   if m:
     timestamp, firmware_version = m.group(1).rstrip().split(',')
