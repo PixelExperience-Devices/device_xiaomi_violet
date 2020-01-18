9296ae38829a312e485c336f2a0bd5d088d09b41
Dyneteve <32036579+Dyneteve@users.noreply.github.com>
2020-01-18 14:19:58 +0200

    violet: Enable TARGET_INCREMENTAL_BLOCK_BASED

--- aosp_violet.mk
+++ aosp_violet.mk
@@ -18,6 +18,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCREMENTAL_BLOCK_BASED := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
