d317ecc54bfa06e290a64c6a38260d954071990b
PIPIPIG233666 <pig.priv@gmail.com>
2020-02-11 23:57:19 -0500

    sm6150-common: tabs -> spaces
    Change-Id: Ie626c6d3df044722b8b1534fbc707710724d3c72

--- sm6150.mk
+++ sm6150.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.	
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
