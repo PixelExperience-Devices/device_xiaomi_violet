0af1a5e01fabb3a1f70cba8277b7a7c7cc67c87a
Pig <pig.priv@gmail.com>
2020-02-17 15:43:03 +0100

    sm6150-common: fod: Remove duplicated setTouchMode onShowFODView
    Change-Id: I8936f97201bff64b7d526b4f974bf929d5e7c3a6

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -94,7 +94,6 @@ Return<void> FingerprintInscreen::onRelease() {
 }
 
 Return<void> FingerprintInscreen::onShowFODView() {
-    TouchFeatureService->setTouchMode(Touch_Fod_Enable, 1);
     this->mFodCircleVisible = true;
     return Void();
 }
