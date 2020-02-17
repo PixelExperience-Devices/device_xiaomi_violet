073c93c7bf485696e20dcea86a8dda685666c33e
Bruno Martins <bgcngm@gmail.com>
2020-02-17 21:02:02 +0000

    sm6150-common: bluetooth: Set BTM_DEF_LOCAL_NAME again
    Bring back the device detection logic, but extended with region check
    so that devices with different naming across markets are properly
    handled.
    
    Change-Id: I0d8348f31cec619fef1d855d4120a672eff247b5

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -5,7 +5,7 @@
  *  for attribution purposes only.
  *
  * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2018-2019 The LineageOS Project
+ * Copyright (C) 2018-2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,6 +30,43 @@
 
 #include "osi/include/osi.h"
 
+typedef struct {
+    const char *product_device;
+    const char *product_region;
+    const char *product_model;
+} device_t;
+
+static const device_t devices[] = {
+    {"davinci", "CN", "Redmi K20"},
+    {"davinci", "GLOBAL", "Mi 9T"},
+    {"davinciin", "IN", "Redmi K20"},
+    {"phoenix", "ALL", "Redmi K30"},
+    {"phoenixin", "IN", "POCO X2"},
+    {"violet", "ALL", "Redmi Note 7 Pro"},
+};
+
+static inline const char *BtmGetDefaultName()
+{
+    char product_device[PROPERTY_VALUE_MAX];
+    char product_region[PROPERTY_VALUE_MAX];
+    property_get("ro.product.device", product_device, "");
+    property_get("ro.boot.hwc", product_region, "");
+
+    for (unsigned int i = 0; i < ARRAY_SIZE(devices); i++) {
+        device_t device = devices[i];
+
+        if (strcmp(device.product_device, product_device) == 0 &&
+               (strcmp(device.product_region, product_region) == 0 ||
+               strcmp(device.product_region, "ALL") == 0)) {
+           return device.product_model;
+       }
+    }
+
+    // Fallback to ro.product.model
+    return "";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
