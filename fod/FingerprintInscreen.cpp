52e6e6d1fa6b56dfc31370c02925ae7ad85974a7
Weikai Kong <pig.priv@gmail.com>
2020-02-24 23:25:51 +0200

    sm6150-common: fod: Enable fod hbm dim layer
    Change-Id: I1fe62d1a063bea1c520265edff61bf3c862b1610

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -28,6 +28,8 @@
 #define PARAM_NIT_630_FOD 1
 #define PARAM_NIT_NONE 0
 
+#define DIM_LAYER_PATH "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/dim_layer_enable"
+
 #define DISPPARAM_PATH "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/disp_param"
 #define DISPPARAM_HBM_FOD_ON "0x20000"
 #define DISPPARAM_HBM_FOD_OFF "0xE0000"
@@ -80,6 +82,7 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
 }
 
 Return<void> FingerprintInscreen::onPress() {
+    set(DIM_LAYER_PATH, 1);
     set(DISPPARAM_PATH, DISPPARAM_HBM_FOD_ON);
     TouchFeatureService->setTouchMode(Touch_Fod_Enable, 1);
     xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_630_FOD);
@@ -94,11 +97,13 @@ Return<void> FingerprintInscreen::onRelease() {
 }
 
 Return<void> FingerprintInscreen::onShowFODView() {
+    set(DIM_LAYER_PATH, 1);
     this->mFodCircleVisible = true;
     return Void();
 }
 
 Return<void> FingerprintInscreen::onHideFODView() {
+    set(DIM_LAYER_PATH, 0);
     set(DISPPARAM_PATH, DISPPARAM_HBM_FOD_OFF);
     TouchFeatureService->resetTouchMode(Touch_Fod_Enable);
     this->mFodCircleVisible = false;
