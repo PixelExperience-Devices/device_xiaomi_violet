6c85aabb0a161f35af777dc96900e77f3bd29f91
Murthy Nidadavolu <mnssr@codeaurora.org>
2020-04-03 12:15:14 +0530

    violet: Remove DRM Widevine and Clearkey HALs
    DRM Widevine and Clearkey HAL entries moved securemsm specific
    makefiles complying FR53051.
    
    Change-Id: I448afcd35279be1fedb3f85fb7476a55a9ca9b77

--- device.mk
+++ device.mk
@@ -198,8 +198,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint feature
