f6b69a88c3d54c50884939e5fefdd757753be9ea
Mike Cailean <mcailean@codeaurora.org>
2020-04-14 12:58:19 -0700

    SV in use mask is updated in the GNSS Adaptor even if the sessions status indicates failure
    Change-Id: I8ed788abc369721e50f7ba853cd51f5d18d828d3
    CRs-fixed: 2660184

--- gnss/GnssAdapter.cpp
+++ gnss/GnssAdapter.cpp
@@ -3434,9 +3434,9 @@ GnssAdapter::needReportForGnssClient(const UlpLocation& ulpLocation,
 bool
 GnssAdapter::needReportForFlpClient(enum loc_sess_status status,
                                     LocPosTechMask techMask) {
-    if ((status == LOC_SESS_INTERMEDIATE) &&
-        !(techMask & LOC_POS_TECH_MASK_SENSORS) &&
-        (!getAllowFlpNetworkFixes())) {
+    if (((LOC_SESS_INTERMEDIATE == status) && !(techMask & LOC_POS_TECH_MASK_SENSORS) &&
+        (!getAllowFlpNetworkFixes())) ||
+        (LOC_SESS_FAILURE == status)) {
         return false;
     } else {
         return true;
@@ -3758,6 +3758,7 @@ GnssAdapter::reportSv(GnssSvNotification& svNotify)
     }
 
     mGnssSvIdUsedInPosAvail = false;
+    mGnssMbSvIdUsedInPosAvail = false;
 }
 
 void
