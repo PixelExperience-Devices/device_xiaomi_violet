3fc229db5c90e01945387d158782d1b343e75dd8
Ayishwarya Narasimhan <ayishwar@codeaurora.org>
2020-04-20 15:27:51 -0700

    violet: config.fs: Add mapping for imsdaemon
    Change-Id: I62629d99e0ace3e5cfa0bdef925e919ddd2ebe12

--- config.fs
+++ config.fs
@@ -16,6 +16,12 @@ value:2905
 [AID_VENDOR_QRTR]
 value:2906
 
+[vendor/bin/imsdaemon]
+mode: 0755
+user: AID_RADIO
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
 [vendor/bin/cnd]
 mode: 0755
 user: AID_SYSTEM
