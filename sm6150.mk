8a34f52668d7fb9facea381e3e0a0f9b1a69ae0c
Pig <pig.priv@gmail.com>
2020-05-08 18:53:02 +0200

    sm6150-common: Decommonize sensors service
     * violet ships sensors V2.
     * Let's use the hidl override feature hence keep 1.0 in main manifest.
    
    Change-Id: I0b506f3cfcf59b0c49372a3c0d70daefef56daa9

--- sm6150.mk
+++ sm6150.mk
@@ -352,8 +352,6 @@ PRODUCT_COPY_FILES += \
 
 # Sensor
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
 # Soong namespaces
