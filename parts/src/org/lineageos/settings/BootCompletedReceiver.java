d014a85f1989a2f28093d89d33070978590ee924
TheScarastic <warabhishek@gmail.com>
2020-05-09 10:21:01 +0200

    sm6150-common: parts: Add dynamic thermal profile implementation
     * this also reverts commit 78a68bd51ef69970ea348fd2fb2db263f441dad1.
    
    Change-Id: I4667b24a8a29a418796dc51b245ede0aca51eea6

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -33,5 +34,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
+        ThermalUtils.startService(context);
     }
 }
