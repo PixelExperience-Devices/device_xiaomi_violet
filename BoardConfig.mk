9c3f7beacc15a0d603e1eadf3a1e56f5fd261bcc
Jaihind Yadav <jaihindyadav@codeaurora.org>
2020-05-20 09:53:29 +0530

    violet: update sepolicy.mk path to point to  vendor sepolicy.mk
    Due to split of sepolicy , Now the vendor Boardconfig.mk should
    call to venodor sepolicy.mk file.
    
    Change-Id: Ie6a930b2310c372b5e570b68372c896ea77ddbd7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,7 +192,7 @@ VENDOR_SECURITY_PATCH := 2020-08-01
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
