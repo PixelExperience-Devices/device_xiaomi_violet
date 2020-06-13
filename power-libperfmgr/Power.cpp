01ba61d8aec85cc083d9674a4a66185f9b5d7db7
Jesse Chan <jc@lineageos.org>
2020-06-13 12:06:26 +0800

    sm6150-common: power-libperfmgr: remove Google-specific display LPM control
    Change-Id: If58f38b99bf6fbd73f6b6ab2144293c63e18b600
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- power-libperfmgr/Power.cpp
+++ power-libperfmgr/Power.cpp
@@ -30,7 +30,6 @@
 
 #include "AudioStreaming.h"
 #include "Power.h"
-#include "display-helper.h"
 #include <linux/input.h>
 
 constexpr int kWakeupModeOff = 4;
@@ -163,13 +162,6 @@ Return<void> Power::powerHint(PowerHint_1_0 hint, int32_t data) {
             }
             break;
         case PowerHint_1_0::LOW_POWER:
-            if (data) {
-                // Device in battery saver mode, enable display low power mode
-                set_display_lpm(true);
-            } else {
-                // Device exiting battery saver mode, disable display low power mode
-                set_display_lpm(false);
-            }
             break;
         default:
             break;
