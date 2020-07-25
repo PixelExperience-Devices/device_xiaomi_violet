7aaa963e44d7993aa12927601e55933fa0c29c7f
merothh <merothh@gmail.com>
2020-07-26 05:15:30 +0530

    sm6150-common: releasetools: append only if board-info for region exists
    Change-Id: Ia76f4808fad69941efd83edafac651a013a7f4b0

--- releasetools.py
+++ releasetools.py
@@ -37,7 +37,9 @@ def AddBasebandAssertion(info, input_zip):
   android_info = input_zip.read("OTA/android-info.txt")
   variants = []
   for variant in ('in', 'cn', 'eea'):
-    variants.append(re.search(r'require\s+version-{}\s*=\s*(\S+)'.format(variant), android_info).group(1).split(','))
+    result = re.search(r'require\s+version-{}\s*=\s*(\S+)'.format(variant), android_info)
+    if result is not None:
+      variants.append(result.group(1).split(','))
   cmd = 'assert(getprop("ro.boot.hwc") == "{0}" && (xiaomi.verify_baseband("{2}", "{1}") == "1" || abort("ERROR: This package requires baseband from atleast {2}. Please upgrade firmware and retry!");) || true);'
   for variant in variants:
     info.script.AppendExtra(cmd.format(*variant))
