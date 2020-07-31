4f84ecb3a1aeeb7f2df9d4676637d3ddda34434f
LuK1337 <priv.luk@gmail.com>
2020-08-01 01:07:57 +0200

    violet: set boot image header version to 1
    Change-Id: I0f8b702c4892addb1896b6be5b7be98c8b5b3097

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,7 @@ BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 # Kernel
+BOARD_BOOT_HEADER_VERSION := 1
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x880000 loop.max_part=7 cgroup.memory=nokmem,nosocket
 BOARD_KERNEL_CMDLINE +=  androidboot.vbmeta.avb_version=1.0
 BOARD_KERNEL_BASE := 0x00000000
