74cfb4f83bd018448df338407807fb771c5442e5
LuK1337 <priv.luk@gmail.com>
2020-08-01 01:07:57 +0200

    violet: Pass proper boot image header version
    Change-Id: I0f8b702c4892addb1896b6be5b7be98c8b5b3097

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,6 +49,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     DTC_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc
 BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/violet
 TARGET_KERNEL_CONFIG := vendor/violet-perf_defconfig
