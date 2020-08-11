05270711db16f132a78ca1c2acbe565d50068fdb
merothh <merothh@gmail.com>
2020-08-11 14:57:07 +0200

    sm6150-common: bluetooth: Drop map based BT name
    * Region checks for {davinci,phoenix}in were wrong, (IN should be INDIA) and it was
      falling back to ro.product.model.
    
      We already set the right model (for every device/region) via libinit,
      hence just drop the map and follow the default behavior.
    
    * This reverts commit 301948dea8dd057e9d1b8014bd6d80cbefabd264, 073c93c7bf485696e20dcea86a8dda685666c33e and 0be25326c189ad809f0c3daa04f9961b4dbbd1c8.
    
    Change-Id: Ifa2867520ad7ea61b919c1d3e3b367796a58cefb

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -22,57 +22,6 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-
-#include <stdint.h>
-#include <string.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-int property_get(const char *key, char *value, const char *default_value);
-#ifdef __cplusplus
-}
-#endif
-
-#include "osi/include/osi.h"
-
-typedef struct {
-    const char *product_device;
-    const char *product_region;
-    const char *product_model;
-} device_t;
-
-static const device_t devices[] = {
-    {"davinci", "CN", "Redmi K20"},
-    {"davinci", "GLOBAL", "Mi 9T"},
-    {"davinciin", "IN", "Redmi K20"},
-    {"phoenix", "ALL", "Redmi K30"},
-    {"phoenixin", "IN", "POCO X2"},
-    {"violet", "ALL", "Redmi Note 7 Pro"},
-};
-
-static inline const char *BtmGetDefaultName()
-{
-    char product_device[92];
-    char product_region[92];
-    property_get("ro.product.device", product_device, "");
-    property_get("ro.boot.hwc", product_region, "");
-
-    for (unsigned int i = 0; i < ARRAY_SIZE(devices); i++) {
-        device_t device = devices[i];
-
-        if (strcmp(device.product_device, product_device) == 0 &&
-               (strcmp(device.product_region, product_region) == 0 ||
-               strcmp(device.product_region, "ALL") == 0)) {
-           return device.product_model;
-       }
-    }
-
-    // Fallback to ro.product.model
-    return "";
-}
-
-#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    32
