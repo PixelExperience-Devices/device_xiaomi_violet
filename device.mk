aee357ae877139cf69af4683c04f92b0480f69db
Shubham Das <dasshubham762@gmail.com>
2020-08-13 21:01:15 +0530

    violet: drop LiveDisplay and Lineage Trust HAL
    Change-Id: Ibf8fb21007c6fc681fda84565619693d883fe668

--- device.mk
+++ device.mk
@@ -279,11 +279,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sm6150
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service.xiaomi_sm6150
-
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
@@ -405,10 +400,6 @@ PRODUCT_PACKAGES += \
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
