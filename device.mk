d5a6c39a6c4254a537a6f5a175802466232754f6
pramod kotreshappa <kpramod@codeaurora.org>
2020-09-01 12:50:22 -0700

    violet: Update IBluetoothAudioProvidersFactory HAL to v2.1
    Change-Id: I396d2a28b8146a7c2018c15fa77452f7011fbac8

--- device.mk
+++ device.mk
@@ -125,7 +125,7 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     libbthost_if \
     android.hardware.bluetooth@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
