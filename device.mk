93ba632faf228ab894fe122f3c1aaf962b4672fc
Pig <pig.priv@gmail.com>
2020-09-13 23:53:56 +0800

    violet: fingerprint: Uprev to v2.2
    Change-Id: Ie4ace90e4d0819c12cb14361dc84ff393afe323d

--- device.mk
+++ device.mk
@@ -211,7 +211,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm6150
+    android.hardware.biometrics.fingerprint@2.2-service.xiaomi_sm6150
 
 # FM
 PRODUCT_PACKAGES += \
