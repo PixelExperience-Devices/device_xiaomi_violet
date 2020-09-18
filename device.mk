88e76eb7752a8a1337c9af5deb66557cdb5442b8
LuK1337 <priv.luk@gmail.com>
2020-09-18 13:12:09 +0200

    violet: Drop android.hardware.thermal HAL
    android.hardware.thermal@1.0::IThermal/default is deprecated in compatibility matrix at FCM Version 4; it should not be served.
    
    Change-Id: Icc3c672a0bf6b9e4ee76ccf91e6cdca5fb778fde

--- device.mk
+++ device.mk
@@ -399,12 +399,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sm6150
-
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
