bf91f09ce6b974f031d981199ff43b36ab3f8c0e
LuK1337 <priv.luk@gmail.com>
2020-09-19 09:51:38 +0800

    violet: Use BOARD_SEPOLICY_M4DEFS to align with QCOM upstream
    Change-Id: I3bca9b0455d9f391ead84ba9ef2a1765afd939dd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -196,6 +196,24 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_SEPOLICY_M4DEFS += \
+    debugfs_clk=vendor_debugfs_clk \
+    debugfs_ipc=vendor_debugfs_ipc \
+    latency_device=vendor_latency_device \
+    nfc_vendor_data_file=vendor_nfc_vendor_data_file \
+    persist_audio_file=vendor_persist_audio_file \
+    persist_sensors_file=vendor_persist_sensors_file \
+    public_vendor_default_prop=vendor_public_vendor_default_prop \
+    sensors_prop=vendor_sensors_prop \
+    sysfs_boot_adsp=vendor_sysfs_boot_adsp \
+    sysfs_devfreq=vendor_sysfs_devfreq \
+    sysfs_fingerprint=vendor_sysfs_fingerprint \
+    sysfs_graphics=vendor_sysfs_graphics \
+    sysfs_kgsl=vendor_sysfs_kgsl \
+    sysfs_scsi_host=vendor_sysfs_scsi_host \
+    sysfs_ssr=vendor_sysfs_ssr \
+    wcnss_service_exec=vendor_wcnss_service_exec \
+    wifi_vendor_data_file=vendor_wifi_vendor_data_file
 
 # WiFi
 BOARD_HOSTAPD_DRIVER := NL80211
