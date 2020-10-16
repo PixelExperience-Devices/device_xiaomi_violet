9a76966e0ce4310b7b19230fa09242bdbfc5a0cd
Pig <pig.priv@gmail.com>
2020-10-17 01:18:16 +0800

    violet: Build new GNSS stack
    Change-Id: If30a30377db91bb70b6adbf5804c984ad27dac72

--- device.mk
+++ device.mk
@@ -221,8 +221,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.0-impl-qti \
-    android.hardware.gnss@2.0-service-qti \
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss@2.1-service-qti \
     libbatching \
     libgeofencing \
     libgnss
