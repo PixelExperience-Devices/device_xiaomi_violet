48e5a487b0bdfee22f0b3321d47a9a73b29ea25d
PIPIPIG233666 <pig.priv@gmail.com>
2020-10-19 23:31:33 -0400

    violet: Enable memcg kernel and socket accounting as per CAF
    Change-Id: Ia736d85e53a94d930263432d95dc1b6068cd0b2d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,7 +45,7 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 1
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x880000 loop.max_part=7 cgroup.memory=nokmem,nosocket
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x880000 loop.max_part=7
 BOARD_KERNEL_CMDLINE +=  androidboot.vbmeta.avb_version=1.0
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
