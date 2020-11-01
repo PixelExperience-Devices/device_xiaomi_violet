6c397b69b670f3e8460331580c5002d20f26a06f
PIPIPIG233666 <pig.priv@gmail.com>
2020-11-01 17:51:18 -0500

    violet: Split component-overrides to support pure aosp/qti bt stack
    Change-Id: Ia40a996ba5164ba8a6fc5dde0dfb3ef859af1477

--- device.mk
+++ device.mk
@@ -142,7 +142,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
+    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
 # Camera
 PRODUCT_PACKAGES += \
