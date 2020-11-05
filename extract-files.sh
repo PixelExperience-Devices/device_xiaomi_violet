5506a56a79f948104ce0fa535cbf30fc8bdd031f
Michael Bestas <mkbestas@lineageos.org>
2020-11-05 19:50:53 +0200

    violet: Switch to patchelf from tools-lineage
    Change-Id: If1a7328804fb9bc39ccec70690f1090f2d0509b3

--- extract-files.sh
+++ extract-files.sh
@@ -57,10 +57,10 @@ fi
 function blob_fixup() {
     case "${1}" in
     vendor/bin/mlipayd@1.1 | vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so )
-        patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
+        "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
     ;;
     system_ext/lib64/libwfdnative.so | system_ext/lib/libwfdnative.so | vendor/lib64/libgoodixhwfingerprint.so )
-        patchelf --remove-needed "android.hidl.base@1.0.so" "${2}"
+        "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
     ;;
     vendor/etc/camera/camxoverridesettings.txt )
         sed -i "s|0x10080|0|g" "${2}"
