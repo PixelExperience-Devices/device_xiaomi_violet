159d6c0c62b77fed2ea4816e9c4d133fa596c900
adi8900 <adrianszymanski242@gmail.com>
2020-11-12 22:44:30 +0100

    violet: Update wifi display blobs list
    * from davinci-user 11 RKQ1.200826.002 20.10.30 release-keys
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -59,7 +59,7 @@ function blob_fixup() {
     vendor/bin/mlipayd@1.1 | vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so )
         patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
     ;;
-    lib64/libwfdnative.so | lib64/libfm-hci.so | lib/libfm-hci.so | vendor/lib64/libgoodixhwfingerprint.so )
+    system_ext/lib64/libwfdnative.so | system_ext/lib/libwfdnative.so | vendor/lib64/libgoodixhwfingerprint.so )
         patchelf --remove-needed "android.hidl.base@1.0.so" "${2}"
     ;;
     vendor/etc/camera/camxoverridesettings.txt )
