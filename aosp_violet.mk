6db68df3a10b31c848693256b264273b4a19509d
Shubham Das <dasshubham762@gmail.com>
2020-11-23 04:25:45 +0000

    violet: Remove build fingerprint hacks
    * Not required anymore with PixelExperience R
    
    Change-Id: Ia98ca4a5e67880cf5295eeaae5228b3c73eec7f7

--- aosp_violet.mk
+++ aosp_violet.mk
@@ -21,10 +21,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="violet-user 9 PKQ1.181203.001 V11.0.8.0.PFHINXM release-keys" \
     PRODUCT_NAME="violet"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
