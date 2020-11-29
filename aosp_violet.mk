b122fb462a46e1f52ab71464fd21fa18ffb19512
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-11-29 00:40:36 -0300

    violet: Drop deprecated flags and use AOSP Recovery
    Change-Id: I3dd875ab99a7e59af6186684da5cd1c9c1263de4

--- aosp_violet.mk
+++ aosp_violet.mk
@@ -11,7 +11,7 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 # Inherit some common Pixel Experience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
+TARGET_USES_AOSP_RECOVERY := true
 IS_PHONE := true
 
 # Device identifier. This must come after all inclusions.
