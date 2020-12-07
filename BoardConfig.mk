feb5dc567d0cfc9ec5bf3b1d9368427e758646ee
Pig <pig.priv@gmail.com>
2020-12-07 10:18:09 +0800

    violet: Move DPM feature property to /system_ext
     * DPM blobs are under /system_ext.
    
    Change-Id: I6bafdc3b310168c6b4d681c9eb94eb4e2e43c5e5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,6 +167,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # QCOM
