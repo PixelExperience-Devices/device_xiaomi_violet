44f92159fe9d9c567c88ea5dcd6ab5d6c8cfa72f
Shubham Das <dasshubham762@gmail.com>
2020-12-19 14:16:18 +0530

    violet: Remove hardware/xiaomi dependencies
    * We are not using anything from it
    
    Change-Id: Ia8bad42e2d7a3b82baf728394d4314f8cbd9cdae
    Signed-off-by: Shubham Das <dasshubham762@gmail.com>

--- device.mk
+++ device.mk
@@ -384,8 +384,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
-    hardware/google/pixel \
-    hardware/xiaomi
+    hardware/google/pixel
 
 # Telephony
 PRODUCT_PACKAGES += \
