5429eed700b7b9ca64214a72fa19316ec23fcaa0
Chenyang Zhong <zhongcy95@gmail.com>
2021-01-01 21:29:20 +0100

    violet: Drop soundtrigger hal
    Change-Id: I8bf82100000e1dc125e19bd2b7b7bf9af790e0eb

--- device.mk
+++ device.mk
@@ -93,7 +93,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio@2.0-service \
     android.hardware.audio@6.0-impl:32 \
-    android.hardware.soundtrigger@2.2-impl:32 \
     audio.a2dp.default \
     audio.primary.sm6150:32 \
     audio.r_submix.default \
