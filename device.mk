535f3d301d09f8c283348c672d515a248d8eabfe
Shubham Das <dasshubham762@gmail.com>
2021-01-26 09:14:46 +0530

    violet: Revert AOSP BT related changes and switch back to QTI BT
     * Switch to CAF audio policy configuration while at it
    
    This reverts commit 92ce9e7de62059c9913c38de94913e387c1bd14b, d8258cab9b7f9df2a7e7ac4b57f0421f123f0e2f.
    
    Change-Id: I9cc28504cb354d311dea33c5312f152a79fd60ba
    Co-authored-by: dianlujitao <dianlujitao@gmail.com>
    Co-authored-by: luk1337 <priv.luk@gmail.com>

--- device.mk
+++ device.mk
@@ -115,7 +115,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
@@ -136,6 +135,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
+    BluetoothQti \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
