16b51be933615ef299e69081dc5f1598d88e32f8
ujjwalkirorimal <ujjwalkirorimal@gmail.com>
2021-10-13 20:50:01 +0530

    violet: Update DRM/Clearkey HIDL to v1.4

--- device.mk
+++ device.mk
@@ -195,8 +195,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.4.vendor \
+    android.hardware.drm@1.4-service.clearkey
 
 # Fingerprint feature
 PRODUCT_COPY_FILES += \
