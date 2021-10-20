802fb44fcde7931b41147486f820917f832dda9c
mikeNG <mikeioannina@gmail.com>
2021-10-20 06:14:44 +0000

    violet: sepolicy: Switch to SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS
    - BOARD_PLAT_{PRIVATE,PUBLIC}_SEPOLICY_DIR has been deprecated.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -193,8 +193,8 @@ VENDOR_SECURITY_PATCH := 2021-05-01
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # WiFi
