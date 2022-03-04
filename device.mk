a561860a30e5705cb542e89ae86f5e7cf0454e48
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:34:57 +0000

    violet: Remove audio A2dp from device.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: I300369d36399ffb558b98bafc2478b872d09faba

--- device.mk
+++ device.mk
@@ -85,7 +85,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl:32 \
-    audio.a2dp.default \
     audio.primary.sm6150:32 \
     audio.r_submix.default \
     audio.usb.default \
