a82a827dbb78a330bdb58a76940a2b5ee9f219b3
Lucchetto <lucchetto.tie@live.com>
2022-08-19 10:39:47 +0200

    violet: Use libhidlbase from vndk v32 for libvendor.goodix.hardware.interfaces.biometrics.fingerprint@2.1.so
    ["_ZN7android8hardware7details17gBnConstructorMapE" and "_ZN7android8hardware7details17gBsConstructorMapE" have been removed on Android 13]
    
    [@Joker-V2: Adapt to our blob]
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -67,6 +67,11 @@ function blob_fixup() {
         sed -i "s|0x10080|0|g" "${2}"
         sed -i "s|0x1F|0x0|g" "${2}"
     ;;
+    # Use VNDK 32 libhidlbase
+    vendor/lib64/libvendor.goodix.hardware.interfaces.biometrics.fingerprint@2.1.so)
+        "${PATCHELF_0_8}" --remove-needed "libhidlbase.so" "${2}"
+        sed -i "s/libhidltransport.so/libhidlbase-v32.so\x00/" "${2}"
+    ;;
     esac
 }
 
