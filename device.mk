ea0934df005a15a0ac1c5cb761ed514623c0cc9a
Bruno Martins <bgcngm@gmail.com>
2022-09-22 00:40:01 +0100

    violet: Drop component overrides
    This is no longer needed as of Android 13.
    
    Change-Id: I38435c47c8781678f82014decbbae40dcab0a07b

--- device.mk
+++ device.mk
@@ -130,9 +130,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
