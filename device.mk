f4d563d68b018edcbe397c7c64c5d98022ee05d0
aleasto <ales.astone@gmail.com>
2023-03-12 17:48:23 +0700

    violet: Build and enable fastbootd
    Since OEM fastboot implementation does not allow us to flash
    critical partitions, fastbootd is useful for upgrading firmware.
    
    Change-Id: I0b20600fe7203a7aec19cbea8f6849052585c6ab

--- device.mk
+++ device.mk
@@ -189,6 +189,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fingerprint feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
