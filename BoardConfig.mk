4b820e4d1d92690cf0ee052479106b5a74b75bc5
ad1tyas1ngh <adityasingh1768@gmail.com>
2023-06-19 13:49:57 +0530

    violet: Drop androidboot.{android_dt_dir,boot_devices}
    Change-Id: Ie486f9258b4939c8bf9766a754ea079553fdc90c
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,7 +50,6 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 BOARD_BOOT_HEADER_VERSION := 1
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x880000 loop.max_part=7 kpti=off
 BOARD_KERNEL_CMDLINE +=  androidboot.vbmeta.avb_version=1.0
-BOARD_KERNEL_CMDLINE += androidboot.android_dt_dir=/non-existent androidboot.boot_devices=soc/7c4000.sdhci
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
