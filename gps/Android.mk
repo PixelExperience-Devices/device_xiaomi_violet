f66cc1f716e54d45fe2034340a8b326893e3d828
Joker-V2 <nipinna0@gmail.com>
2023-10-05 18:57:35 +0200

    violet: gps: Drop `Werror`

--- gps/Android.mk
+++ gps/Android.mk
@@ -2,7 +2,6 @@ ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 
 # Set required flags
 GNSS_CFLAGS := \
-    -Werror \
     -Wno-error=unused-parameter \
     -Wno-error=macro-redefined \
     -Wno-error=reorder \
