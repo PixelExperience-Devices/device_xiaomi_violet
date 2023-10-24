3d9ce60708b2251d7b811a3bcd6ac519af5428de
Bruno Martins <bgcngm@gmail.com>
2023-10-24 18:12:15 +0100

    violet: Migrate to restructured sepolicy_vndr
    The new folder structure now looks like this:
    
      device/qcom/sepolicy_vndr
      ├─ legacy-um (meant for sdm845 until sm8350)
      ├─ sm8450
      └─ sm8550
    
    SEPolicy.mk placed in the root automatically redirects devices to the
    proper subfolder.
    
    Change-Id: I4ed1a06af779909d64172da294c4a99a3ae9ba0d
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -179,7 +179,7 @@ VENDOR_SECURITY_PATCH := 2021-05-01
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy_vndr/legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
